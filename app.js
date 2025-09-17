e41d7f95e7845e3dcd4531c2adba80e8f5c52b0a
leirbagAoContrario <ueoka.gabriel@escola.pr.gov.br>
2025-09-17 16:26:10 -0300

    17/09

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     console.log(categoria, pergunta, resposta)
 
     let container = document.getElementById('container')
-    let cartao - document.createElement('article')
+    let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
     cartao.innerHTML =
