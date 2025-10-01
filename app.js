7c46f0bd7c505c367daca5a112ea9be899d83df1
leirbagAoContrario <ueoka.gabriel@escola.pr.gov.br>
2025-10-01 15:18:42 -0300

    1/10

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ function criaCartao(categoria, pergunta, resposta) {
 
     cartao.innerHTML =
         <div class="cartao__conteudo">
-        <h3>${categoria} </h3>
+            <h3>${categoria} </h3>
 
             <div class="cartao__conteudo__pergunta">
-        <p>${pergunta}</p>
+                <p>${pergunta}</p>
             </div>
 
-                <div class="cartao__conteudo__resposta">
-        <p>${resposta}</p>
-                </div>
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
         </div>
-        container.appendChild(cartao)
+
+    container.appendChild(cartao)
 }
